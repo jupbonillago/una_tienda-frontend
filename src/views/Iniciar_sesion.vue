707ab2f2f63af506cd943ba3f97b555b4ebeb794
sebcamacho <vscamachos@unal.edu.co>
2021-06-28 12:48:57 -0500

    Captcha implementado

--- src/views/Iniciar_sesion.vue
+++ src/views/Iniciar_sesion.vue
@@ -9,6 +9,7 @@
         <div id="login" class="login">
           <b-form
             @submit="validate"
+            @submit.prevent="submit"
             class="datosIS col-xs-12 col-sm-8 col-md-6 col-lg-4 border border-primary rounded-lg"
           >
             <h2 class="col-12 text-center text-primary mt-3 mb-3">Login</h2>
@@ -41,11 +42,26 @@
                 type="password"
               ></b-form-input>
             </b-form-group>
-            <div class="text-center" align-v="center">
-              <b-button size="lg" type="sumbit" variant="primary"
-                >Iniciar Sesión</b-button
+
+            <div
+                :class="{mx_recaptcha_failed: !recaptcha}"
+            >
+              <vue-recaptcha
+                  sitekey="6Lf8XmEbAAAAAFCxwbvOYCk5y3jgfFyGriVF1Xdz"
+                  @verify="mxVerify"
               >
+              </vue-recaptcha>
+              <br/>
+              <small>Complete el captcha!!!</small>
+
             </div>
+
+              <div class="text-center" align-v="center">
+                <b-button size="lg" type="sumbit" variant="primary"
+                >Iniciar Sesión</b-button
+                >
+              </div>
+
             <br />
             <div class="text-center" align-v="center">
               <router-link :to="{ name: 'forgotPassword' }"
@@ -67,54 +83,66 @@
 </template>
 
 <script>
+
+import VueRecaptcha from 'vue-recaptcha';
 import axios from 'axios'
 import {mapMutations} from 'vuex';
   const path = "/oauth/token";
 
     export default {
         name: "Iniciar_sesion.vue",
-        components: {},
+        components: { VueRecaptcha },
         data(){
             return {
                 username: '',
-                password: ''
+                password: '',
+                recaptcha: null,
             }
         },
         methods: {
+            mxVerify( response ){
+              // console.log( response )
+              this.recaptcha = response
+            },
             ...mapMutations(['login']),
             validate( event ){
+              if(this.recaptcha == null){
+                console.log( this.recaptcha )
+                event.preventDefault()
+              }else {
                 axios
-                .post( this.$store.state.backURL + path, // URL
-                    { }, // Body
-                    {
-                        headers: {
-                          'Content-Type': 'application/json'
-                        },
-                        params: {
+                    .post(this.$store.state.backURL + path, // URL
+                        {}, // Body
+                        {
+                          headers: {
+                            'Content-Type': 'application/json'
+                          },
+                          params: {
                             username: this.username,
                             password: this.password,
                             grant_type: 'password'
-                        },
-                        auth: {
+                          },
+                          auth: {
                             username: "soft-eng-ii",
                             password: "secret",
+                          }
                         }
-                    }
-                ).then( response => {
-                    if( response.status !== 200 ){
-                        alert( "Error en la autenticación" );
-                    }else{
-                        this.login(response.data.access_token);
-                        this.$router.push( {name: 'home'} );
-                    }
-                } ).catch( error => {
-                    if( error.response.status === 400 ){
-                      alert( "Credenciales incorrectas" );
-                    }else{
-                      alert( "¡Parece que hubo un error de comunicación con el servidor!" );
-                    }
-                } );
+                    ).then(response => {
+                  if (response.status !== 200) {
+                    alert("Error en la autenticación");
+                  } else {
+                    this.login(response.data.access_token);
+                    this.$router.push({name: 'home'});
+                  }
+                }).catch(error => {
+                  if (error.response.status === 400) {
+                    alert("Credenciales incorrectas");
+                  } else {
+                    alert("¡Parece que hubo un error de comunicación con el servidor!");
+                  }
+                });
                 event.preventDefault();
+              }
             }
         },
     }
@@ -141,5 +169,14 @@ import {mapMutations} from 'vuex';
 .datosIS {
   background: white;
   color: #054c6b;
+}
+
+small {
+  color: red;
+  display: none;
+}
+
+.mx_form_inv .mx_recaptcha_failed small {
+  display: block;
 }
 </style>
